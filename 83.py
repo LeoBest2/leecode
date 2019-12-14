02b85fe653dd1b79ea8fb560cf7c01e4ae19dd35
Leo <qianxunlan@qq.com>
2019-12-14 13:01:12 +0800

    Add 83. 删除排序链表中的重复元素

--- 83.py
+++ 83.py
@@ -19,3 +19,39 @@
     @License: MIT
 """
 
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def list_to_node(li: [int]):
+    ret = ListNode(None)
+    ret_bak = ret
+    for _ in li:
+        ret.next = ListNode(_)
+        ret = ret.next
+    return ret_bak.next
+
+
+def print_node(node: ListNode):
+    while node is not None:
+        print(node.val, end=' -> ')
+        node = node.next
+    print('')
+
+
+def deleteDuplicates(head: ListNode) -> ListNode:
+    head_bak = head
+    while head and head.next is not None:
+        if head.val == head.next.val:
+            head.next = head.next.next
+        else:
+            head = head.next
+    return head_bak
+
+
+print_node(deleteDuplicates(list_to_node([1, 1, 2])))
+print_node(deleteDuplicates(list_to_node([1, 1, 2, 3, 3])))
+print_node(deleteDuplicates(list_to_node([1, 1, 1, 1, 1])))
